2efb6e802abe4d9e3193a43bcef349018f5b2f1e
Othmam-Al-Ani <142592326+Othmam-Al-Ani@users.noreply.github.com>
2024-02-09 16:31:16 +0100

    card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import reactLogo from "../assets/react.svg";
 import "../App.css";
+import Mail from ".././assets/mail.png";
+import Phone from ".././assets/phone.png";
+import LinkedIn from ".././assets/LinkedIn.png";
+import "../pages/Contact.css";
+import "../pages/Home.css";
 
 function Card(data) {
-  const { titel, description, image } = data;
+  const { titel, mail, number,linked, image } = data;
 
   const [Ishover, setIshover] = useState(false);
 
@@ -35,15 +40,30 @@ function Card(data) {
           {titel}{" "}
         </h2>
         {Ishover && (
-          <p
-            style={{
+          
+              <div id="person_info_form_div" style={{
               opacity: Ishover ? 1 : 0,
               transition: "opacity 1s ease",
               fontSize: 12,
-            }}
-          >
-            {description}
-          </p>
+            }}>
+                <div id="info_form">
+                  <img src={Mail} alt="" id="icon" />
+                  <h5>{mail}</h5>
+                </div>
+                <div id="info_form">
+                  <img src={Phone} alt="" id="icon" />
+                  <h5>{number}</h5>
+                </div>
+                <div id="info_form">
+                  <img src={LinkedIn} alt="" id="icon" />
+                  <a href="https://www.linkedin.com/company/afilum/">
+                    <h5>{linked}</h5>
+                  </a>
+                  
+                </div>
+              </div>
+            
+          
         )}
       </div>
     </div>
